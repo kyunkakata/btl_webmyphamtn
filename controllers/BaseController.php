0688fe6d88ee5df1b535d439a85178556352382e
tanpm <tanpm@mobiletech.vn>
2020-06-29 22:27:26 +0700

    [init] Login + SignUp + table name

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -4,6 +4,13 @@ require_once('models/BaseModel.php');
 class BaseController {
 public $model;
 public $view;
+
+public $TBL_PRODUCT = 'product';
+public $TBL_CART = 'tbl_cart';
+public $TBL_EXTRA_INFO_PRODUCT = 'extra_product_info';
+public $TBL_ORDER = "tbl_order";
+public $TBL_USER = 'user';
+
 public function __construct(){
     $this->model = new BaseModel();
     $this->view = new BaseView();
@@ -37,7 +44,76 @@ public function CartList() {
     return $this->view->CartList();
 }
 
+/**
+ * Dang nhap vao he thong.
+ * Neu la user thuong thi se chuyen toi man hinh trang chu.
+ * Neu la admin thi se chuyen toi man hinh quan ly.
+ */
+public function doSignIn() {
+    header('Location: ?task=HomePage');
+}
 
+/**
+ * Dang ky vao he thong.
+ * Dang ki xong chuyen toi man hinh dang nhap.
+ */
+public function doSignUp() {
+    header('Location: ? task=LogIn');
+}
+
+/**
+ * lay danh sach san pham dua theo cac truong thuoc tinh, 
+ * value la gia tri so sanh
+ * compare la phep so sanh: bao gom largerThan (lớn hơn), smallerThan (nhỏ hơn), equals(bằng với)
+ */
+public function getListProductBy($tag, $value, $compare){
+
+}
+
+/**
+ * them san pham vao gio hang dua theo user_id
+ */
+public function addProductToCart($product, $user_id) {
+
+}
+
+/**
+ * Xoa toan bo san pham trong gio hang dua theo user_id
+ */
+public function removeAllCart($user_id){
+
+}
+
+/**
+ * Lay toan bo san pham trong gio hang dua theo user_id
+ */
+public function getProductByUserId($user_id){
+
+}
+
+/**
+ * Tao ra list danh sach cac san pham can dat mua (dua vao ket qua chon loc tren UI va gio hang) 
+ */
+public function initPaymentListProduct($list_products) {
+
+}
+/**
+ * Lay ra danh sach cac san pham da dat hang dua theo user_id
+ */
+public function getPaidListProductsByUserId($user_id){
+
+}
+
+/**
+ * Lay ra danh sach toan bo cac san pham da dat hang.
+ */
+public function getPaidListProducts(){
+
+}
+
+public function alert($msg) {
+    alert($msg);
+}
 // public function delete() {
 // 	$table ='tbl_diachi';
 // 	$id = $_GET['id'];
